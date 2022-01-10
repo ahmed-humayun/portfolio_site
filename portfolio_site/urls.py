d1bf5e2aa9e9106402295f3a59f1c0dd59ee78b0
ahmed-humayun <93235542+ahmed-humayun@users.noreply.github.com>
2022-01-10 15:57:42 -0500

    worked on css and js

--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from quote_generator import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
 ]
